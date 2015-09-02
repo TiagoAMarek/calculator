2e8b910df41488e02ae1f6eb016087912de8772e
TiagoAMarek <tiagomarek@gmail.com>
2015-09-02 16:51:52 -0300

    working on calcs

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -9,6 +9,34 @@ var UICalculator = function() {
         PUBLIC  = this,
         op      = new Operations();
 
+    PRIVATE.calculate = function (str) {
+        var total = 0;
+        str = str.split("");
+        var bla = str.length % 2;
+        if(bla !== 0){
+            for (var i = 0; i < str.length; i++) {
+                if(/[\+\*\/\-]/.test(str[i])) {
+                    switch(str[i]){
+                        case '+':
+                            total = op.sum(str[i-1], str[i+1]);
+                            break;
+                        case '-':
+                            total = op.subtract(str[i-1], str[i+1]);
+                            break;
+                        case '*':
+                            total = op.multiply(str[i-1], str[i+1]);
+                            break;
+                        case '/':
+                            total = op.divide(str[i-1], str[i+1]);
+                            break;
+                    }
+                    str[i+1] = total;
+                }
+            }
+        }
+        return total;
+    };
+
     PRIVATE.keyboardListener = function () {
         var keyContainer    = document.querySelector(".keyboard"),
             displayContent  = document.querySelector(".display-content");
@@ -20,7 +48,14 @@ var UICalculator = function() {
         keyContainer.addEventListener('touchend', function(evt) {
             var target = evt.target;
             if(target.tagName.toLowerCase() === "button") {
-                displayContent.innerHTML += target.innerHTML;
+                if(target.innerHTML !== '=') {
+                    displayContent.innerHTML += target.innerHTML;
+                } else {
+                    if(/[\+\*\/\-]/.test(displayContent.innerHTML)){
+                        var total = PRIVATE.calculate(displayContent.innerHTML);
+                        displayContent.innerHTML = total;
+                    }
+                }
             }
         });
     };
--- www/tests/tests.js
+++ www/tests/tests.js
@@ -5,5 +5,5 @@ QUnit.test( "Operations", function( assert ) {
     assert.equal( op.subtract(5, 2), 3, "Minus operation working" );
     assert.equal( op.sum(5, 2), 7, "Sum operation working" );
     assert.equal( op.multiply(5, 2), 10, "Multiplication operation working" );
-    assert.equal( op.divide(5, 2), 2.5, "Division operation working" );
+    assert.equal( op.divide(14, 2), 7, "Division operation working" );
 });
