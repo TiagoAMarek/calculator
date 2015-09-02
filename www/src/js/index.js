a2fc05333f09d3676a45ac659e6bd16e3b757f7d
TiagoAMarek <tiagomarek@gmail.com>
2015-09-01 21:50:24 -0300

    making the keyboard works, showing the numbers on display

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -17,10 +17,10 @@ var UICalculator = function() {
             throw new Error("An error has ocurred when looking for the keyboard or display");
         }
 
-        keyContainer.addEventListener('touchend', function(evt){
+        keyContainer.addEventListener('touchend', function(evt) {
             var target = evt.target;
             if(target.tagName.toLowerCase() === "button") {
-                displayContent.firstChild.innerHTML += target.innerHTML;
+                displayContent.innerHTML += target.innerHTML;
             }
         });
     };
