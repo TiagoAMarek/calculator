cbce0937d8dba68251c6ed367002c5ff15f48d9f
TiagoAMarek <tiagomarek@gmail.com>
2015-09-03 14:39:11 -0300

    working on calcs

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -10,22 +10,31 @@ var UICalculator = function() {
         op      = new Operations();
 
     PRIVATE.calculate = function (str) {
-        var total = 0;
+        var total = 0,
+            nextValue = 0,
+            prevValue = 0;
+
         str = str.split(" ");
+        var lastIndex = str.length - 1;
+        str.pop(lastIndex);
+
         for (var i = 0; i < str.length; i++) {
-            if(/[\+\*\/\-]/.test(str[i]) && str.length > i) {
+            nextValue = i+1;
+            prevValue = i-1;
+
+            if(/[\+\*\/\-]/.test(str[i]) && str.length > nextValue) {
                 switch(str[i]){
                     case '+':
-                        total = op.sum(str[i-1], str[i+1]);
+                        total = op.sum(str[prevValue], str[nextValue]);
                         break;
                     case '-':
-                        total = op.subtract(str[i-1], str[i+1]);
+                        total = op.subtract(str[prevValue], str[nextValue]);
                         break;
                     case '*':
-                        total = op.multiply(str[i-1], str[i+1]);
+                        total = op.multiply(str[prevValue], str[nextValue]);
                         break;
                     case '/':
-                        total = op.divide(str[i-1], str[i+1]);
+                        total = op.divide(str[prevValue], str[nextValue]);
                         break;
                 }
                 str[i+1] = total;
